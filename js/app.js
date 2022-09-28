ac0cb749a8215ad08b10b73141daf06ddb144473
SaraMCG <84095824+SaraMCG@users.noreply.github.com>
2022-09-27 23:14:11 -0500

    Adding dots

--- js/app.js
+++ js/app.js
@@ -1,19 +1,19 @@
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    navigator.serviceWorker.register('./sw.js');
 }
 
 if (window.caches) {
     caches.open('cache-v1').then((cache) => {
         cache.addAll(
             [
-                '/',
-                '/index.html',
-                '/css/styles.css',
-                '/js/app.js',
-                '/pages/division.html',
-                '/pages/multiplicacion.html',
-                '/pages/resta.html',
-                '/pages/suma.html',
+                './',
+                './index.html',
+                './css/styles.css',
+                './js/app.js',
+                './pages/division.html',
+                './pages/multiplicacion.html',
+                './pages/resta.html',
+                './pages/suma.html',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js'
             ]
--- sw.js
+++ sw.js
@@ -3,14 +3,14 @@ self.addEventListener('install', (event) => {
     const promiseCache = caches.open('cache-v1').then((cache) => {
         return cache.addAll(
             [
-                '/',
-                '/index.html',
-                '/css/styles.css',
-                '/js/app.js',
-                '/pages/division.html',
-                '/pages/multiplicacion.html',
-                '/pages/resta.html',
-                '/pages/suma.html',
+                './',
+                './index.html',
+                './css/styles.css',
+                './js/app.js',
+                './pages/division.html',
+                './pages/multiplicacion.html',
+                './pages/resta.html',
+                './pages/suma.html',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
                 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js'
             ]
