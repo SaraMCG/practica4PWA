5d8bc82e248100cb85bb0036c1d32676a9f5e574
SaraMCG <84095824+SaraMCG@users.noreply.github.com>
2022-09-27 23:00:35 -0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,23 @@
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('/sw.js');
+}
+
+if (window.caches) {
+    caches.open('cache-v1').then((cache) => {
+        cache.addAll(
+            [
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/js/app.js',
+                '/pages/division.html',
+                '/pages/multiplicacion.html',
+                '/pages/resta.html',
+                '/pages/suma.html',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js'
+            ]
+        )
+    })
+}
+
---
+++ sw.js
@@ -0,0 +1,26 @@
+self.addEventListener('install', (event) => {
+    console.log('SW: Instalado');
+    const promiseCache = caches.open('cache-v1').then((cache) => {
+        return cache.addAll(
+            [
+                '/',
+                '/index.html',
+                '/css/styles.css',
+                '/js/app.js',
+                '/pages/division.html',
+                '/pages/multiplicacion.html',
+                '/pages/resta.html',
+                '/pages/suma.html',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css',
+                'https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js'
+            ]
+        );
+    })
+    event.waitUntil(promiseCache);
+})
+
+
+self.addEventListener('fetch', (event) => {
+    const respCache = caches.match(event.request);
+    event.respondWith(respCache);
+})
